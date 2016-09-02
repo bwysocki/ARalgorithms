77522eda9efd987a99cdcd2cfca438090bee7f33
Bartosz Wysocki <Q1WY@nykredit.dk>
2016-09-02 08:38:00 +0200

    Threshold.

--- js/image-transformations.js
+++ js/image-transformations.js
@@ -11,11 +11,19 @@ class ITImage {
 
 class ITOperations {
 	
+	/*
+		Canvas image to gray scale represented as ITIImage. Example:
+		
+		let ctx=c.getContext("2d");
+		ctx.drawImage( document.getElementById("sample") ,0,0);
+		img = ctx.getImageData(0, 0, 465, 620);;
+		IT.toGrayscale(img);
+	*/
 	toGrayscale(imageSrc) {
 		let imageDst = new ITImage(), src = imageSrc.data, j = 0;
 
 		for (let i = 0; i < src.length; i += 4){
-			imageDst.data[j ++] = src[i] * 0.2989 + src[i + 1] * 0.5870 + src[i + 2] * 0.1140;
+			imageDst.data[j ++] = (src[i] * 0.2989 + src[i + 1] * 0.5870 + src[i + 2] * 0.1140) & 0xff; //omits decimal signs
 		}
 	  
 	  imageDst.width = imageSrc.width;
@@ -24,6 +32,14 @@ class ITOperations {
 	  return imageDst;
 	};
 	
+	/*
+		Transform ITImage to canvas image. Example:
+		
+		let gray = IT.toGrayscale(img);
+		let imgData = ctx.createImageData(465,620);
+		IT.grayITImageToCanvasImageData(gray, imgData);
+		ctx.putImageData(imgData,0,0);
+	*/
 	grayITImageToCanvasImageData(grayITImage, imgData) {
 
 		let j = 0, i = 0, dst = imgData.data;
@@ -34,7 +50,29 @@ class ITOperations {
 			dst[i+3] = 255;;
 		}
 
-	}
+	};
+	
+	/*
+		Transforms gray scaled image src to thresholded version (with fixed threshold rate). Example:
+		
+		let gray = IT.threshold(IT.toGrayscale(img), 150);
+	*/
+	threshold(imageSrc, threshold){
+		let imageDst = new ITImage(), src = imageSrc.data, thresholdMap = [];
+
+		for (let i = 0; i < 256; ++ i){
+			thresholdMap[i] = i <= threshold? 0: 255;
+		}
+
+		for (let i = 0; i < src.length; ++ i){
+			imageDst.data[i] = thresholdMap[ src[i] ];
+		}
+
+		imageDst.width = imageSrc.width;
+		imageDst.height = imageSrc.height;
+
+		return imageDst;
+	};
 	
 }
 
--- js/main.js
+++ js/main.js
@@ -7,7 +7,9 @@ $( document ).ready( function () {
 
 	img = ctx.getImageData(0, 0, 465, 620);;
 
-	let gray = IT.toGrayscale(img);
+	let gray = IT.threshold(IT.toGrayscale(img), 150);
+	
+	
 	
 	var imgData=ctx.createImageData(465,620);
 	IT.grayITImageToCanvasImageData(gray, imgData);
